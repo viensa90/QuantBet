09d4ef5be05dc8fe456a8d638da9b4c834ecf439
viensa90 <viensa90@gmail.com>
2026-08-08 18:36:50 -0300

    Agregadas nuevas funciones y carpetas al proyecto

--- src/notifications/__init__.py
+++ src/notifications/__init__.py
@@ -1,15 +1,2 @@
-"""
-src/notifications/__init__.py
-Módulo de notificaciones para QuantBet
-Versión: 0.3.1
-"""
-
-from src.notifications.email_notifier import EmailNotifier
-from src.notifications.telegram_notifier import TelegramNotifier
-from src.notifications.notification_manager import NotificationManager
-
-__all__ = [
-    'EmailNotifier',
-    'TelegramNotifier',
-    'NotificationManager'
-]
+# src/notifications/__init__.py
+# (vacío intencionadamente – los notificadores se importan explícitamente)

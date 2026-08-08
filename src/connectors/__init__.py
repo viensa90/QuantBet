b578be7f0b72f0bef517bda97accccdf8337448b
viensa90 <viensa90@gmail.com>
2026-08-08 18:22:15 -0300

    Agregadas nuevas funciones y carpetas al proyecto

--- src/connectors/__init__.py
+++ src/connectors/__init__.py
@@ -1,10 +1,3 @@
-# src/connectors/__init__.py
-"""
-Módulo de conectores para QuantBet.
-Principio: Los Conectores solo obtienen datos, nunca deciden.
-"""
-
 from .base import IDataProvider
 from .csv_provider import CSVProvider
-
-__all__ = ['IDataProvider', 'CSVProvider']
+from .odds_api_provider import OddsAPIProvider
--- src/storage/__init__.py
+++ src/storage/__init__.py
@@ -1,7 +1,1 @@
-"""
-Módulo de almacenamiento de QuantBet.
-"""
-from .database import Database, get_db
-from .repository import Repository
-
-__all__ = ['Database', 'get_db', 'Repository']
+from .database import Database
